1ef8809cd89fbbab99109af52c497c14bb358813
Amine <amine@anava.tn>
2020-06-27 23:18:24 +0100

    feat(settings): add required models, routes, controllers

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UserSeeder::class);
         $this->call(ProductSeeder::class);
         $this->call(IngredientSeeder::class);
+        $this->call(SettingSeeder::class);
     }
 }
