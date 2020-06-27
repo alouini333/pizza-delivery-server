6d48fcdfa1c10ba243b2795bb2f0a156233389e6
Amine <amine@anava.tn>
2020-06-27 12:00:11 +0100

    feat: update user seeder

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -11,6 +11,34 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $users = factory(App\User::class, 5)->create();
+        $user = new User;
+        $user->name = 'John';
+        $user->last_name = 'Doe';
+        $user->email = 'John.Doe@example.com';
+        $user->password = 'secret';
+        $user->gender = 'male';
+        $user->phone = '1122334455';
+        $user->save();
+
+        $user = new User;
+        $user->name = 'Florian';
+        $user->last_name = 'Gerste';
+        $user->email = 'Florian.Gerste@example.com';
+        $user->password = 'secret';
+        $user->gender = 'male';
+        $user->phone = '1122334455';
+        $user->address = 'some address';
+        $user->save();
+
+        $user = new User;
+        $user->name = 'Klaudia';
+        $user->last_name = 'Wolf';
+        $user->email = 'Klaudia.Wolf@example.com';
+        $user->password = 'secret';
+        $user->gender = 'female';
+        $user->phone = '1122334455';
+        $user->address = 'some address';
+        $user->save();
+
     }
 }
