f06947655b8a37ecc1075593c068960369061afb
Amine <amine@anava.tn>
2020-07-01 07:18:29 +0100

    feat: update setting seeder

--- database/seeds/SettingSeeder.php
+++ database/seeds/SettingSeeder.php
@@ -14,7 +14,7 @@ class SettingSeeder extends Seeder
     {
         $setting = new Setting;
         $setting->name = 'Pizza Haus';
-        $setting->logo = 'https://via.placeholder.com/50x50';
+        $setting->logo = 'https://i.ibb.co/8Yv3zqR/logo.png';
         $setting->delivery_fees = 12;
         $setting->footer = 'Welcome to Pizza Haus';
         $setting->save();
